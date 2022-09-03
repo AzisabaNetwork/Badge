de92fe77e5195385f38a54951ec926b0abbdfe8e
testusuke <silverbro007@icloud.com>
2022-09-03 14:13:04 +0900

    feat: add config file and db setting

--- src/main/kotlin/net/testusuke/badge/Main.kt
+++ src/main/kotlin/net/testusuke/badge/Main.kt
@@ -1,5 +1,8 @@
 package net.testusuke.badge
 
+import net.testusuke.badge.badges.BadgeStatusManager
+import net.testusuke.badge.utils.DataBase
+import net.testusuke.badge.utils.DataBaseSetting
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
@@ -10,15 +13,34 @@ class Main: JavaPlugin() {
 
     companion object {
         lateinit var plugin: Main
+        lateinit var database: DataBase
     }
 
     override fun onEnable() {
         plugin = this
+        //  load configuration
+        saveDefaultConfig()
 
+        //  load DB
+        database = DataBase(
+            this,
+            DataBaseSetting(
+                host = config.getString("database.host") ?: "localhost",
+                user = config.getString("database.user") ?: "root",
+                pass = config.getString("database.pass") ?: "password",
+                port = config.getString("database.port") ?: "3306",
+                db = config.getString("database.db") ?: "db"
+            )
+        )
+
+        //  register all command executor
+        registerCommand()
+
+        //  load player status
+        BadgeStatusManager.init()
     }
 
-    /**
-     * ComponentBuilderを作る
-     * BadgeBuilderの中にComponentBuilderを入れておき、Badge.build()時にcomponentもbuildする！
-     */
+    private fun registerCommand() {
+        getCommand("")
+    }
 }
